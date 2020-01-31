c4910fbd063e45e001cb87f494d92bb54ecbfdcc
Owen Shade <2378587s@student.gla.ac.uk>
2020-01-31 19:34:41 +0000

    Chapter 6 completed

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
+    return render(request, 'rango/index.html', context = context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -17,13 +26,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
-def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
 
-    return render(request, 'rango/index.html', context = context_dict)
     
     
 def about(request):
