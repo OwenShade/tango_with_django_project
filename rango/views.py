9935bbe70f9ba94ac26bbb9d52d27f4191f04113
Owen Shade <2378587s@student.gla.ac.uk>
2020-02-02 02:06:21 +0000

    Chapter 10 Complete.

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -14,8 +15,11 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    
+    visitor_cookie_handler(request)
 
-    return render(request, 'rango/index.html', context = context_dict)
+    response = render(request, 'rango/index.html', context = context_dict)
+    return response
 
 
 def show_category(request, category_name_slug):
@@ -73,7 +77,9 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    visitor_cookie_handler(request)
+    context_dict={'visits': request.session['visits']}
+    return render(request, 'rango/about.html', context=context_dict)
 
 def register(request):
     registered = False
@@ -124,6 +130,26 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
 
 @login_required
 def restricted(request):
